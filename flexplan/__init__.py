20d40700c50f943b255d77f47b78b29d86b07d51
leavers <leavers@users.noreply.github.com>
2024-08-21 22:55:49 +0800

    build: add publish action (#4)
    * build: add publish action
    
    * build: update publish action
    
    * chore: update action token
    
    * chore: update publish action

--- flexplan/__init__.py
+++ flexplan/__init__.py
@@ -4,7 +4,7 @@ from flexplan.workbench.base import Workbench
 from flexplan.workers.base import Worker
 from flexplan.workshop import Workshop
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 __all__ = (
